22ee25fca9f5c37222095bbda06af1ecbffce821
Angela Ahn <29741357+aahn84@users.noreply.github.com>
2018-02-27 13:46:27 -0800

    add signup and login modals

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-document.querySelector('#signupButton').addEventListener('click', displaySignup);
-document.querySelector('#signupButton').addEventListener('touchstart', displaySignup);
+const signupButton = document.querySelector('#signupButton');
+signupButton.addEventListener('click', displaySignup);
+signupButton.addEventListener('touchstart', displaySignup);
 
-document.querySelector('#loginButton').addEventListener('click', displaySignup);
-document.querySelector('#loginButton').addEventListener('touchstart', displaySignup);
+const signupClose = document.querySelector('#signupClose');
+signupClose.addEventListener('click', hideSignup);
+signupClose.addEventListener('touchstart', hideSignup);
+
+const loginButton = document.querySelector('#loginButton');
+loginButton.addEventListener('click', displayLogin);
+loginButton.addEventListener('touchstart', displayLogin);
+
+const loginClose = document.querySelector('#loginClose');
+loginClose.addEventListener('click', hideLogin);
+loginClose.addEventListener('touchstart', hideLogin);
 
 
 function displaySignup() {
   document.querySelector('#signup').style.display = "block";
 }
 
+function hideSignup() {
+  document.querySelector('#signup').style.display = "none";
+}
+
 function displayLogin() {
   document.querySelector('#login').style.display = "block";
+}
+
+function hideLogin() {
+  document.querySelector('#login').style.display = "none";
 }
